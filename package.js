0a9df567f34dc38037600cffff0cce4e1635b50c
limingth <limingth@gmail.com>
2015-10-28 00:43:32 +0800

    Change title to Chinese

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'maodouio:posts',
-  version: '0.0.5',
+  version: '0.0.6',
   // Brief, one-line summary of the package.
   summary: 'Maodou.io posts package, A meteor package that provides posts pages at /posts',
   // URL to the Git repository containing the source code for this package.
