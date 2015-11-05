167845c20ccbefd3a6a34624631e21639a234521
limingth <limingth@gmail.com>
2015-11-05 14:10:07 +0800

    Fix bug of no helper showTimeAgo etc..

--- client/posts/posts_index.js
+++ client/posts/posts_index.js
@@ -18,3 +18,23 @@ Template.postsIndex.events ({
     }
   }
 });
+
+// Outputs e.g. 12 days ago or 2 hours ago
+UI.registerHelper('showTimeAgo', function(date) {
+  return !date ? "" : moment(date).fromNow();
+});
+
+// Outputs e.g. Jan, 2013
+UI.registerHelper('showMonthYear', function(date) {
+  return !date ? "" : moment(date).format("MMM, YYYY");
+});
+
+// Outputs e.g. 12th Jan, 2013
+UI.registerHelper('showDayMonthYear', function(date) {
+  return !date ? "" : moment(date).format("Do MMM, YYYY")
+});
+
+// Outputs August 30th 2014, 5:33:46 pm
+UI.registerHelper('showPrettyTimestamp', function(date) {
+  return !date ? "" : moment(date).format("MMMM Do YYYY, h:mm:ss a")
+});
